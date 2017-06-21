0b6072085f52f531bf32cd933795395d65c2f17c
ozawa <ozawa@i-hearts.jp>
2017-06-21 18:34:56 +0900

    reducerを追加

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import {combineReducers} from "redux"
+import text from "./Text"
+
+const reducer = combineReducers({
+    text
+});
+
+export default reducer;
---
+++ src/reducers/text.js
@@ -0,0 +1,29 @@
+// 初期値の設定をしてあげる
+const initialState = {
+    flag: true,
+    text: "入力してください",
+    number: 0
+}
+
+// action で受け取った値を state に適用して更新する
+export default function text(state = initialState, action) {
+    const { flag, text, number } = action;
+    switch (action.type) {
+        case "SUBMIT":
+            // 今回ここでは状態の更新だけだが、action の値によってさらに別な値も変えたりするなど
+            return Object.assign({}, state, {
+                text: text,
+                number: number
+            });
+        case "SHOW":
+            return Object.assign({}, state, {
+                flag: true
+            });
+        case "HIDE":
+            return Object.assign({}, state, {
+                flag: false
+            });
+        default:
+           return state;
+    }
+}
